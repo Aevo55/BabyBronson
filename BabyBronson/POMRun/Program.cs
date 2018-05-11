56928fde3481f3f44a30c5875e5fdc9bfe1b645a
SEAN-LAPTOP\Sean <s.dunlop@socyinc.com>
2018-05-11 10:55:25 -0400

    I didn't do anything

--- BabyBronson/POMRun/Program.cs
+++ BabyBronson/POMRun/Program.cs
@@ -50,9 +50,11 @@ namespace POMRun
             while (gmailhome.GetUnreadEmails().Count > 0) {
              currentemail = gmailhome.clickUnreadEmail(1);
             user = lookup.GetUser(connectionString, currentemail.getAlias());
-            email.sendMail(destinationaddress, "Forwarding Service",loginpassword, "FWD:" + currentemail.getSubject(), user.MakeString() + currentemail.getInnerHTML());
+            email.sendMail("s.dunlop@socyinc.com", "Forwarding Service", "TestPass", "FWD:" + currentemail.getSubject(), user.MakeString() + currentemail.getInnerHTML());
             gmailhome = currentemail.returnToInbox();   
             }
+            
+
             //driver.Close();
         }
 
